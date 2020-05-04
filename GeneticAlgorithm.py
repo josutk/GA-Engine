21ce5287db48213b01322013190df7fbca1e8094
Josué Nascimento <josuetk63@gmail.com>
2020-05-04 12:29:11 -0400

    Template to GeneticAlgorithm

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,19 @@
+class GeneticAlgorithmTemplate:
+
+    def __init__(self):
+        pass
+    
+    def fitness_function(self):
+        pass
+    
+    def mutation(self):
+        pass
+
+    def parent_selection(self):
+        pass
+
+    def cross_over(self):
+        pass
+
+    def offspring(self):
+        pass
