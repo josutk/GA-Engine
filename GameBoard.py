176ff5fba9320c21cd50aa1274dcbc19a57f27f6
Josué Nascimento <josuetk63@gmail.com>
2020-05-04 12:59:28 -0400

    Set center positon to create board screen

--- GameBoard.py
+++ GameBoard.py
@@ -1,10 +1,12 @@
 import pygame
+import os 
 class GameBoard:
 
     def __init__(self, height=0, weight=0):
         self.height = height
         self.weight = weight
-    
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (500, 200)
+
     def start_board(self):
         screen = pygame.display.set_mode((self.height, self.weight))
         return screen
