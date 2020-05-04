6772e0d8bd9f75495546057ea4e01258b26256a2
Josué Nascimento <josuetk63@gmail.com>
2020-05-04 10:16:23 -0400

    Add Scene

---
+++ Scene.py
@@ -0,0 +1,13 @@
+class Scene:
+
+    def __init__(self, id=0):
+        self.id = id
+
+    def update(self, events):
+        pass
+
+    def load(self):
+        pass
+
+    def draw(self, screen):
+        pass
