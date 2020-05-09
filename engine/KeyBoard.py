9245b6c7bcaa5fc4b3871e1ae9cf925a68a2b70a
Josué Nascimento <josuetk63@gmail.com>
2020-05-09 18:35:17 -0400

    Add press button control

--- engine/KeyBoard.py
+++ engine/KeyBoard.py
@@ -10,6 +10,10 @@ class KeyBoard:
    
     def __init__(self):
         self.events = pygame.event.get()
+        self.keys = pygame.key.get_pressed()
 
     def get_events(self):
         return self.events
+
+    def press_botton(self):
+       return self.keys 
