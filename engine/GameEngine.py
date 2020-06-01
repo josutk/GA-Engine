b335d2e7907339a07361717a3e90dd749c3d84e4
Josué Nascimento <josuetk63@gmail.com>
2020-06-01 15:09:54 -0300

    Move files

--- engine/GameEngine.py
+++ engine/GameEngine.py
@@ -1,20 +1,21 @@
 import pygame
 import sys
-from model_game.ExperimentalScene import ExperimentalScene
+#from model_game.ExperimentalScene import ExperimentalScene
 
 class GameEngine:
 
-	def __init__(self, screen):
+	def __init__(self, screen, scene):
 		self.screen = screen
+		self.scene = scene	
 	
 	def run(self):
 		pygame.init()
-		scene = ExperimentalScene(self.screen, 0)		
-		scene.load()
+		#scene = ExperimentalScene(self.screen, 0)		
+		self.scene.load()
 		while True:
 			self.screen.fill((255,255,255))
-			scene.draw()
-			scene.update()
+			self.scene.draw()
+			self.scene.update()
 			for event in pygame.event.get(): 
 				if event.type == pygame.QUIT:
 					sys.exit()
