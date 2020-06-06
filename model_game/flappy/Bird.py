d892938a3de72c95dc126d06ada34282c7532474
Josué Nascimento <josuetk63@gmail.com>
2020-06-06 13:22:36 -0300

    Improve bird movement

--- model_game/flappy/Bird.py
+++ model_game/flappy/Bird.py
@@ -2,7 +2,7 @@ from engine.GameObject import GameObject
 from engine.Sprite import Sprite
 from engine.KeyBoard import KeyBoard
 
-SPEED = 3
+SPEED = 135
 GRAVITY = 1
 class Bird(GameObject):
 
@@ -31,17 +31,16 @@ class Bird(GameObject):
             if event.type == keyBoard.KEYDOWN:
                 if event.key == keyBoard.SPACE:
                     flag_movement = 'up'
-        print(flag_movement)
         self.movement(flag_movement)
         self.group.update()        
 
     def movement(self, flag):
         if flag == 'up':
-            self.position_y = -1
+            self.position_y = -SPEED + self.position_y
             self.sprite_1.set_position(self.position_y)
         else:
             self.position_y += GRAVITY
             self.sprite_1.set_position(self.position_y)
-        
+        print(self.position_y)
     def draw(self):
         self.group.draw(self.screen)
