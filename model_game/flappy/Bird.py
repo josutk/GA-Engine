b10746fcb943de299f55d3d2cc079659de73067d
Josué Nascimento <josuetk63@gmail.com>
2020-06-06 13:24:48 -0300

    Remove unnused code

--- model_game/flappy/Bird.py
+++ model_game/flappy/Bird.py
@@ -8,21 +8,14 @@ class Bird(GameObject):
 
     def __init__(self, screen, position_x, position_y, height, width):
         super().__init__(screen, position_x, position_y)
-        self.sprite_1 = Sprite('bluebird-upflap.png', height, width, None)
-        self.sprite_2 = Sprite('bluebird-midflap.png', height, width, None)
-        self.sprite_3 = Sprite('bluebird-downflap.png', height, width, None)
+        self.sprite = Sprite('bluebird-upflap.png', height, width, None)
         
-        self.group = self.sprite_1.group()
-        self.sprites = [self.sprite_1, 
-                        self.sprite_2, 
-                        self.sprite_3
-                        ]
+        self.group = self.sprite.group()
         
-        self.sprite_1_image = self.sprite_1.get_image()
         self.position_y = position_y
     
     def load(self):
-        self.group.add(self.sprite_1)
+        self.group.add(self.sprite)
     
     def update(self):
         keyBoard = KeyBoard()
@@ -37,10 +30,9 @@ class Bird(GameObject):
     def movement(self, flag):
         if flag == 'up':
             self.position_y = -SPEED + self.position_y
-            self.sprite_1.set_position(self.position_y)
+            self.sprite.set_position(self.position_y)
         else:
             self.position_y += GRAVITY
-            self.sprite_1.set_position(self.position_y)
-        print(self.position_y)
+            self.sprite.set_position(self.position_y)
     def draw(self):
         self.group.draw(self.screen)
