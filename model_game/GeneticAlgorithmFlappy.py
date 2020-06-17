60d39613f48ab51eb828fbedfe4a87105c3563ca
Josué Nascimento <josuetk63@gmail.com>
2020-06-17 12:20:45 -0300

    Generate new Population

--- model_game/GeneticAlgorithmFlappy.py
+++ model_game/GeneticAlgorithmFlappy.py
@@ -9,20 +9,25 @@ class GeneticAlgorithmFlappy(GeneticAlgorithmTemplate):
     def __init__(self, population_size):
         self.population_size = population_size
         self.generation = []
+        self.indexs = None
+
+    def neural_network(self):
+        model = Sequential()
+        model.add(Dense(units=6, input_shape=(3,) , name='inpute'))
+        model.add(Dense(units=1, input_shape=(6,), activation='sigmoid', name='out'))
+        return model
 
     def population(self):
         for idx in range(population_size):
-            model = Sequential()
-            model.add(Dense(units=6, input_shape=(3,) , name='inpute'))
-            model.add(Dense(units=1, input_shape=(6,), activation='sigmoid', name='out'))
-            self.generation.append(model)
+           model = self.neural_network()
+           self.generation.append(model)
         
     def fitness(self, scores):
         np_scores = np.array(scores)
-        indexs = np_scores.argsort()[-self.population_size/2:][::-1]
+        self.indexs = np_scores.argsort()[-self.population_size/2:][::-1]
         return indexs
 
-    def crossover(self):
+    def crossover(self, idx1, idx2):
         weights1 = self.population[idx1].get_weights()
         weights2 = self.population[idx2].get_weights()
         newWeight1 = weights2 
@@ -42,3 +47,10 @@ class GeneticAlgorithmFlappy(GeneticAlgorithmTemplate):
                         mutation = random.uniform(-0.8, 0.8)
                         weights[i][j] -= mutation
         return weights
+
+    def new_population(self):
+        new_generation = self.generation[self.indexs]
+        for idx in range(len(self.population_size)/2):
+            model = self.neural_network()
+            new_generation.append(model)
+        self.generation = new_generation
