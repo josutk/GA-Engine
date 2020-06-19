4b301a9a399f05d721d4f1eb90786756f4f3bd05
Josué Nascimento <josuetk63@gmail.com>
2020-06-19 11:13:24 -0300

    Reload Screen after all birds die

--- engine/GameEngine.py
+++ engine/GameEngine.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
+#import BirdException
+from copy import copy 
 
 class GameEngine:
 
 	def __init__(self, screen, scene):
 		self.screen = screen
 		self.scene = scene	
-	
+		self.screen_copy = copy(screen)
+
 	def run(self):
 		pygame.init()
 		clock = pygame.time.Clock()
@@ -20,3 +23,23 @@ class GameEngine:
 				if event.type == pygame.QUIT:
 					sys.exit()
 			pygame.display.update()
+
+	def run_continuos(self):
+		pygame.init()
+		clock = pygame.time.Clock()
+		self.scene.load()
+		reload_scene = False
+		while True:
+			clock.tick(60)
+			try:
+				if reload_scene:
+					self.scene.load()
+					reload_scene = False
+				self.scene.draw()
+				self.scene.update()
+				for event in pygame.event.get(): 
+					if event.type == pygame.QUIT:
+						sys.exit()
+				pygame.display.update()
+			except Exception:
+				reload_scene = True
